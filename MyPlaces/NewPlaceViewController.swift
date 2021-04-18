20e57ce3da06e2a8dff7ef40dc6f0f76cdb5308b
StrangeC0de <demonsoul2007@yandex.ru>
2021-04-18 14:07:48 +0300

    New Place View Controller Working with keyboard, text fields and lables

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Pisarev Dmitriy on 18.04.2021.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView()
+    }
+    
+    //MARK: Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+            
+    }
+
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    //Скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
